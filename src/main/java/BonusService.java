8b903f8415cd857fc37299de6a752d7dfa917850
Aleksandr <artemyev86an@gmail.com>
2022-04-29 15:55:09 +0500

    ДобавленБонусТест

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,11 +1,11 @@
 public class BonusService {
-
-        public int calculate(int amount, boolean isRegistered) {
-            int percent = isRegistered ? 3 : 1;
-            int bonus = amount * percent / 100;
-            if (bonus > 500) {
-                bonus = 500;
-            }
-            return bonus;
+    public long calculate(long amount, boolean isRegistered) {
+        int percent = isRegistered ? 3 : 1;
+        long bonus = amount * percent / 100 / 100;
+        long limit = 500;
+        if (bonus > limit) {
+            bonus = limit;
         }
+        return bonus;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
 
-        int actual = service.calculate( amount: 1000, );
+        long actual1 = service.calculate( 1000, true);
+        int expected1 = 30;
+        System.out.println("OP:" + expected1 + "ФР:" + actual1);
+
+        long actual2 = service.calculate( 10_000_000, true);
+        int expected2 = 500;
+        System.out.println("OP:" + expected2 + "ФР:" + actual2);
     }
 }

710f769f4bd71852920c6881b3bbf6145cb43edf
Aleksandr <artemyev86an@gmail.com>
2022-04-29 14:01:56 +0500

    start

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        int actual = service.calculate( amount: 1000, );
+    }
+}
